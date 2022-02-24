6e2ce15278e44eb1d62ff29bafb0fd196ed3ddcf
GiovanniCalia <johnca@outlook.it>
2022-02-24 16:01:01 +0100

    Continuazione esercizio

---
+++ Gioco dei dadi/js/script.js
@@ -0,0 +1,10 @@
+let randomNumberPlayer = Math.floor(Math.random() * 7);
+console.log(randomNumberPlayer);
+
+let randomNumberComputer = Math.floor(Math.random() * 7);
+console.log(randomNumberComputer);
+
+/*document.getElementById(`random-number-player`).innerHTML = `Number player:` + 
+document.getElementById(`random-number-computer`).innerHTML = `Number player:` +
+document.getElementById(`winner`).innerHTML = `The winner is: ` +*/
+
