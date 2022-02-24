bd4ba82e46fabe72267cfe4259b0bb5e9f96eee5
GiovanniCalia <johnca@outlook.it>
2022-02-24 16:12:42 +0100

    Completamento esercizio dadi

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -4,7 +4,16 @@ console.log(randomNumberPlayer);
 let randomNumberComputer = Math.floor(Math.random() * 7);
 console.log(randomNumberComputer);
 
-/*document.getElementById(`random-number-player`).innerHTML = `Number player:` + 
-document.getElementById(`random-number-computer`).innerHTML = `Number player:` +
-document.getElementById(`winner`).innerHTML = `The winner is: ` +*/
+
+if (randomNumberPlayer > randomNumberComputer){
+    document.getElementById(`winner`).innerHTML = `The winner is: player`;
+} else if (randomNumberPlayer < randomNumberComputer){
+    document.getElementById(`winner`).innerHTML = `The winner is: computer`;
+} else {
+    document.getElementById(`winner`).innerHTML = `Draw`;
+}
+
+
+document.getElementById(`random-number-player`).innerHTML = `Number player: ` + randomNumberPlayer;
+document.getElementById(`random-number-computer`).innerHTML = `Number computer: ` + randomNumberComputer;
 
